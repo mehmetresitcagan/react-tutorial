88e97f1ab1ab85c4ecd5d808cb53b21cb6e0fa4b
mehmetresitcagan <mresitcagan@gmail.com>
2024-04-01 19:53:27 +0300

    counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <p>{counter}</p>
+      <hr />
+      <br />
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <br />
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+    </>
+  );
 };
 
 export default App;

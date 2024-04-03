e895d8a3a32bfcff5055daca4e1cc9e23c55ffd0
mehmetresitcagan <mresitcagan@gmail.com>
2024-04-03 14:50:06 +0300

    react router tutuorial - contact app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import ErrorPage from "./error-page";
+import EditContact, { action as editAction } from "./routes/edit";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
